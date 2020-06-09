c451d3da2cb914df132ed5d8af76360c81162302
Elise Rousseau <rousseaujumelle@gmail.com>
2020-06-09 22:30:54 +0200

    portfolio

---
+++ js/reseaux.js
@@ -0,0 +1,31 @@
+
+// let reseau= document.getElementById('reseau')
+// let fas = reseau.getElementsByClassName("fas")
+// let fab = reseau.getElementsByClassName("fab")
+
+// let mail = document.getElementById("mail")
+// let addresse = document.getElementById("addresse")
+// let numero = document.getElementById("numero")
+// let insta = document.getElementById("insta")
+
+// function mouseOver(reseau, valeur){
+// 	reseau.addEventListener("mouseover", function(){
+// 	valeur.style.display = "block";
+// })
+// }
+
+// function mouseOut(reseau, valeur){
+// 	reseau.addEventListener("mouseout", function(){
+// 	valeur.style.display = "none";
+// })
+// }
+
+// mouseOver(fas[0], mail)
+// mouseOver(fas[1], addresse)
+// mouseOver(fas[2], numero)
+// mouseOver(fab[0], insta)
+
+// mouseOut(fas[0], mail)
+// mouseOut(fas[1], addresse)
+// mouseOut(fas[2], numero)
+// mouseOut(fab[0], insta)
---
+++ js/scroll.js
@@ -0,0 +1,137 @@
+const sr = ScrollReveal();
+
+sr.reveal('h1', {
+	origin : 'top',
+	distance:'100px',
+	duration : 2000,
+	
+	reset:true,
+
+})
+
+sr.reveal('#header p', {
+	origin : 'bottom',
+	distance:'30px',
+	duration : 2000,
+	scale: 0.5,
+	reset:true,
+	delay : 500,
+})
+
+sr.reveal('#photo', {
+	origin : 'right',
+	distance:'60px',
+	duration : 2000,
+	delay : 500,
+	reset:true,
+});
+sr.reveal('#coordonnees', {
+	origin : 'left',
+	distance:'40px',
+	duration : 2000,
+	delay : 500,
+	reset:true,
+});
+
+
+sr.reveal('.contenuQualites p', {
+	origin : 'left',
+	distance:'30px',
+	duration : 2000,
+	delay : 500,
+	reset:true,
+});
+
+
+sr.reveal('.buttonInfosCompetences', {
+	origin : 'top',
+	distance:'1px',
+	duration : 1000,
+	scale: 0.5,
+	reset:true,
+	delay:500,
+});
+
+sr.reveal('.coordonnee', {
+	origin : 'top',
+	distance:'2px',
+	duration : 1500,
+	scale: 0.5,
+	reset:true,
+	delay:1000,
+});
+sr.reveal('#age', {
+	origin : 'left',
+	distance:'200px',
+	duration : 1500,
+	scale: 0.5,
+	reset:true,
+	delay:1000,
+});
+
+sr.reveal('#titrePortfolio', {
+	origin : 'top',
+	distance:'40px',
+	duration : 1000,
+	
+	reset:true,
+	delay:500,
+});
+
+
+
+sr.reveal('.projet', {
+	origin : 'top',
+	distance:'2px',
+	duration : 1000,
+	scale: 0.5,
+	reset:true,
+	
+},500);
+
+sr.reveal('#ulCandidature', {
+	origin : 'top',
+	distance:'100px',
+	duration : 1000,
+	
+	reset:true,
+
+});
+sr.reveal('#sectionLettreMotivation', {
+	origin : 'bottom',
+	distance:'100px',
+	duration : 1000,
+	
+	reset:true,
+	delay:600,
+});
+
+sr.reveal('#labelsRebriqueCv', {
+	origin : 'top',
+	distance:'100px',
+	duration : 1000,
+	
+	reset:true,
+	delay:600,
+});
+sr.reveal('#logoOC img', {
+	origin : 'bottom',
+	distance:'100px',
+	duration : 1000,
+	
+	reset:true,
+	delay:600,
+});
+
+sr.reveal('#b ', {
+	origin : 'top',
+	distance:'30px',
+	duration : 1000,
+	
+	reset:true,
+	delay:600,
+});
+
+
+
+
